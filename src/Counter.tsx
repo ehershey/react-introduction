7670b41972b8e869fc680d7a637731df725e9d62
Konstantin Tennhard <konstantin@tennhard.net>
2024-09-05 20:59:22 -0400

    Reducer based state management

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+function createCounter(
+  state: { count: number },
+  action: "increment" | "decrement"
+) {
+  const { count } = state;
+
+  switch (action) {
+    case "decrement":
+      return { ...state, count: count - 1 };
+    case "increment":
+      return { ...state, count: count + 1 };
+  }
+}
 
 function useCounter() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const [{ count }, dispatch] = useReducer(createCounter, { count: 0 });
+  const increment = () => dispatch("increment");
+  const decrement = () => dispatch("decrement");
 
   return {
     count,
